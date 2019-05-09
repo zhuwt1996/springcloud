9ff66b7f09c60d3dea757bea3935bde40f7794b0
zhuwutao <1255284280@qq.com>
2019-05-09 14:55:21 +0800

    架子

---
+++ shop-consumer-dept-80/src/main/java/com/pagoda/springcloud/DeptConsumer80_App.java
@@ -0,0 +1,13 @@
+package com.pagoda.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeptConsumer80_App
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(DeptConsumer80_App.class, args);
+    }
+}
