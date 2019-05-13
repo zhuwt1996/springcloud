6189f8f2ada7d0cc4a9ddc094e6f159d2dcd3a0d
zhuwutao <1255284280@qq.com>
2019-05-13 10:13:46 +0800

    hystrix dashboard服务监控

---
+++ shop-consumer-dept-hystrix-dashboard/src/main/java/com/pagoda/springcloud/DeptConsumer_DashBoard_App.java
@@ -0,0 +1,16 @@
+package com.pagoda.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * Created by zhuwutao on 2019/5/13
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class DeptConsumer_DashBoard_App {
+    public static void main(String[] args) {
+        SpringApplication.run(DeptConsumer_DashBoard_App.class,args);
+    }
+}
